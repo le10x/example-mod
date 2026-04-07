deda7d289dcadd2a35a6911cf84ac7ef37fc5838
le10x <leanderdrb@gmail.com>
2026-04-07 16:39:44 -0400

    Remove MyPauseLayer implementation from main.cpp
    Removed custom MyPauseLayer class and its functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,6 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PauseLayer.hpp>
-
 using namespace geode::prelude;
 
-class $modify(MyPauseLayer, PauseLayer) {
-    bool init() {
-        if (!PauseLayer::init()) return false;
-
-        auto menu = this->getChildByID("left-button-menu");
-        if (!menu) return true;
-
-        auto sprite = CCSprite::createWithSpriteFrameName("GJ_plainBtn_001.png");
-        auto btn = CCMenuItemSpriteExtra::create(
-            sprite, this, menu_selector(MyPauseLayer::onMyButton)
-        );
-
-        btn->setID("hello-world-btn"_spr);
-        menu->addChild(btn);
-        menu->updateLayout();
-
-        return true;
-    }
-
-    void onMyButton(CCObject*) {
-        FLAlertLayer::create("Geode", "Hello World", "OK")->show();
-    }
-};
+$execute {
+    log::info("Hola desde Geode!");
+}
