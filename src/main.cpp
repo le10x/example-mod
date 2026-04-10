cb35e49c1de322f529ae01df6c032f1476745a61
le10x <leanderdrb@gmail.com>
2026-04-10 12:28:08 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,47 @@
 using namespace geode::prelude;
 
 class $modify(MyPauseLayer, PauseLayer) {
+    
+    void onToggleTwoPlayer(CCObject* sender) {
+        auto levelSettings = PlayLayer::get()->m_levelSettings;
+        
+        // Alternamos el valor
+        levelSettings->m_twoPlayerMode = !levelSettings->m_twoPlayerMode;
+
+        // Actualizamos el texto del botón para que el usuario vea el cambio
+        auto btn = static_cast<ButtonSprite*>(static_cast<CCMenuItemSpriteExtra*>(sender)->getNormalImage());
+        std::string label = levelSettings->m_twoPlayerMode ? "2 Player: [X]" : "2 Player: [ ]";
+        btn->setString(label.c_str());
+        
+        // Mensaje rápido en pantalla
+        Notification::create(
+            levelSettings->m_twoPlayerMode ? "2 Player Mode Enabled" : "2 Player Mode Disabled",
+            NotificationIcon::Info
+        )->show();
+    }
+
     bool init(bool unfocused) {
         if (!PauseLayer::init(unfocused)) return false;
 
-        // 1. Revisamos si la configuración está activa
-        bool isEnabled = Mod::get()->getSettingValue<bool>("activate");
+        if (!Mod::get()->getSettingValue<bool>("show-button")) return true;
 
-        if (isEnabled) {
-            // 2. Buscamos el botón de comentarios y los menús laterales
-            auto commentBtn = this->getChildByIDRecursive("comments-button");
-            auto leftMenu = this->getChildByID("left-button-menu");
-            auto rightMenu = this->getChildByID("right-button-menu");
+        auto levelSettings = PlayLayer::get()->m_levelSettings;
 
-            if (commentBtn && leftMenu && rightMenu) {
-                // 3. Lo quitamos de la izquierda y lo pasamos a la derecha
-                commentBtn->removeFromParentAndCleanup(false);
-                rightMenu->addChild(commentBtn);
-                
-                // 4. Actualizamos ambos layouts para que no se amontonen
-                leftMenu->updateLayout();
-                rightMenu->updateLayout();
-            }
+        // Texto dinámico dependiendo del estado actual
+        std::string labelText = levelSettings->m_twoPlayerMode ? "2 Player: [X]" : "2 Player: [ ]";
+
+        auto buttonSprite = ButtonSprite::create(labelText.c_str(), "goldFont.fnt", "GJ_button_04.png", 0.6f);
+        
+        auto myButton = CCMenuItemSpriteExtra::create(
+            buttonSprite,
+            this,
+            menu_selector(MyPauseLayer::onToggleTwoPlayer)
+        );
+        myButton->setID("two-player-toggle"_spr);
+
+        if (auto leftMenu = this->getChildByID("left-button-menu")) {
+            leftMenu->addChild(myButton);
+            leftMenu->updateLayout();
         }
 
         return true;
